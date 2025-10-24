de76acc29a5573891842722c6c55d1928bf63257
aui1122 <nmichaelandrei@gmail.com>
2025-10-24 11:17:18 +0800

    feat: add responsive Navbar, Footer, LayoutWrapper

---
+++ src/components/layout/LayoutWrapper.tsx
@@ -0,0 +1,17 @@
+
+"use client"
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function LayoutWrapper({ children }: {children: React.ReactNode }) { 
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
